2a5818ca0ad560471cae6a81387c2c9ca1552413
Ana <asimicev@gmail.com>
2024-03-14 00:23:29 +0100

    automate organization of files in downloads folder according to their extension

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import os
 import os.path
+import shutil
 
-downloads_path = "C:/Users/asimi/Desktop/downloads_path"
+downloads_path = "C:/Users/asimi/Downloads"
 
-if not os.path.exists(downloads_path):
-    os.makedirs(downloads_path)
-    print("Created")
+filetypes = {
+    "Audio": [".m4a", ".wav", ".flac", ".mp3", ".wma"],
+    "Vid": [".mp4", ".mov", ".wmv", ".avi", ".flv", ".mkv"],
+    "Img": [".gif", ".jpeg", ".jpg", ".png", ".svg"],
+    "Docs": [".doc", ".docx", ".html", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx", ".txt", ".epub"],
+    "Other": [".exe"]
+}
 
-else:
-    print("Exists")
+for item in os.listdir(downloads_path):
+    for key in filetypes:
+        for extension in filetypes[key]:
+            if item.endswith(extension):
+                if not os.path.exists(f"{downloads_path}/{key}"):
+                    os.makedirs(f"{downloads_path}/{key}")
+                    shutil.move(f"{downloads_path}/{item}", f"{downloads_path}/{key}/{item}")
+
+                else:
+                    shutil.move(f"{downloads_path}/{item}", f"{downloads_path}/{key}/{item}")
+            else:
+                print("Unknown extension.")
